97fd4d52fffc621d540b5214382e89f8eaae9014
BitCamp <BitCamp@BitCamp-PC>
2016-09-20 11:21:44 +0900

    .

--- sample02/src/main/java/test/Hello.java
+++ sample02/src/main/java/test/Hello.java
@@ -7,6 +7,9 @@ public class Hello {
     System.out.println("별이되어라라라라라라랄");
     System.out.println("충돌나라");
     System.out.println("아아아아아아아");
+    
+    System.out.println("싱크로?");
+
     int j = 10;
     for (int i = 0; i < 10; i++) {
       System.out.println(i);
