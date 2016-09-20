4f41a63427c7c39612d3771fcbc10b7b30008a9f
BitCamp <BitCamp@BitCamp-PC>
2016-09-20 11:09:28 +0900

    .

--- sample02/src/main/java/test/Hello.java
+++ sample02/src/main/java/test/Hello.java
@@ -2,14 +2,10 @@ package test;
 
 public class Hello {
   public static void main(String[] args) {
-<<<<<<< HEAD
-=======
     
     System.out.println("Hello1");
     System.out.println("별이되어라라라라라라랄");
     System.out.println("아아아아아아아");
-  }
->>>>>>> branch 'master' of https://github.com/Ryu-se-hyun/repo1.git
 
     for (int i = 0; i < 10; i++) {
       System.out.println(i);
