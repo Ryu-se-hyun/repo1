e5d09e12c23f000e62421a343d3397de0999cbdb
BitCamp <BitCamp@BitCamp-PC>
2016-09-20 10:44:48 +0900

    .

--- sample02/src/main/java/test/Hello.java
+++ sample02/src/main/java/test/Hello.java
@@ -2,9 +2,9 @@ package test;
 
 public class Hello {
   public static void main(String[] args) {
-    
-    System.out.println("Hello1");
-    
-  }
 
+    for (int i = 0; i < 10; i++) {
+      System.out.println(i);
+    }    
+  }
 }
